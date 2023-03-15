6742ec5b206bec4b69418eecfb4bd8608834a28b
testershmester <katerina.zharskaya@mail.ru>
2023-03-15 20:26:06 +0300

    SD-4_jenkins: update chrome driver options

--- src/test/java/by/teachmeskills/BaseTest.java
+++ src/test/java/by/teachmeskills/BaseTest.java
@@ -34,6 +34,7 @@ public class BaseTest {
             WebDriverManager.chromedriver().setup();
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--headless=new");
+            options.addArguments("--remote-allow-origins=*");
             driver = new ChromeDriver(options);
         }
 
